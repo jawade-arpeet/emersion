9262fd7da33932d223241eddb0e68d02accc3998
jawade.arpeet <jawade.arpeet@gmail.com>
2024-12-31 14:15:19 +0530

    add multiple button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,18 +1,40 @@
 import React from "react";
 
+import { cva, type VariantProps } from "class-variance-authority";
+
 import cn from "@/utils/cn";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "outline";
-}
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
 
-function Button({ children, className, ...props }: ButtonProps) {
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-foreground text-background hover:bg-stone-300",
+        outline: "border border-stone-800 bg-background hover:bg-stone-800",
+        link: "border border-stone-800  bg-background hover:bg-stone-800",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+function Button({ children, variant, size, className, ...props }: ButtonProps) {
   return (
     <button
-      className={cn(
-        "inline-flex items-center justify-center px-4 py-2 h-9 text-sm font-medium rounded-md bg-foreground text-background",
-        className
-      )}
+      className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     >
       {children}
